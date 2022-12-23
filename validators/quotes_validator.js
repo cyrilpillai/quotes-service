231563c4c117d2a4c943400ed6ea2860eb4e072e
cyrilpillai <cyrilpillai@gmail.com>
2022-12-23 13:21:38 +0530

    Add minimum character validation

--- validators/quotes_validator.js
+++ validators/quotes_validator.js
@@ -4,10 +4,18 @@ const titleLabel = 'title';
 const descriptionLabel = 'description';
 const authorLabel = 'author';
 
+const options = {
+    errors: {
+        wrap: {
+            label: ''
+        }
+    }
+};
+
 const quoteBodySchema = Joi.object({
-    title: Joi.string().required().label(titleLabel),
+    title: Joi.string().required().min(3).label(titleLabel),
     description: Joi.string().label(descriptionLabel).allow(''),
-    author: Joi.string().required().label(authorLabel),
+    author: Joi.string().required().min(3).label(authorLabel),
 });
 
 function createErrorBody(code, description) {
@@ -15,17 +23,18 @@ function createErrorBody(code, description) {
 }
 
 export default function validateQuoteSchema(value) {
-    const { error, value: result } = quoteBodySchema.validate(value);
+    const { error, value: result } = quoteBodySchema.validate(value, options);
 
     if (error === undefined) {
         result.success = true;
         return result;
     } else {
-        switch (error.details[0].context.label) {
+        const reason = error.details[0];
+        switch (reason.context.label) {
             case titleLabel:
-                return createErrorBody('1001', 'title is required');
+                return createErrorBody('1001', reason.message);
             case authorLabel:
-                return createErrorBody('1003', 'author is required');
+                return createErrorBody('1002', reason.message);
             default:
                 return createErrorBody('5000', 'unknown error');
         }
